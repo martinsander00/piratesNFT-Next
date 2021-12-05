857d2da2d32471d6cd0c332699618fed8ec83230
Carlos Lorenzo <ce.lorenzol@gmail.com>
2021-12-05 20:33:52 +0100

    Navigation bar updated and landing banner created and positioned

--- components/landingBannerComponent.js
+++ components/landingBannerComponent.js
@@ -1,13 +1,31 @@
 import Image from 'next/image'
+import {motion} from 'framer-motion'
+import bgBanner from '../public/landingBanner/BG_BANNER.jpg'
+import logoWeb from '../public/landingBanner/LOGO_WEB.png'
+import longRocks from '../public/landingBanner/LONG_ROCKS.png'
+import redPlanet from '../public/landingBanner/RedPlanet.png'
+
+import styles from '../styles/landingBanner.module.scss';
 
 const LandingBanner = () => {
 	const path = '/home/'
 	return(
-		<div className="bg-banner">
-		<h1>Landing Banner</h1>
+		<div className={styles.lb}>
+			<img src="landingBanner/BG_BANNER.jpg" className={styles.lb__bgBanner} />
+			<img src="landingBanner/LOGO_WEB.png" className={styles.lb__logoWeb} />
+			<img src="landingBanner/LONG_ROCKS.png" className={styles.lb__longRocks} />
+			<img src="landingBanner/RedPlanet.png" className={styles.lb__redPlanet} />
 		</div>
 	);
 
 }
+
+export const ImageBlock = ({ children , className }) => {
+	return (
+		<motion.div className={className}>
+			{children}
+		</motion.div>
+	);
+};
 
 export default LandingBanner;
--- components/navigationComponent.js
+++ components/navigationComponent.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -11,76 +11,36 @@ import styles from '../styles/navigation.module.scss';
 
 
 const Navigation = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [size, setSize] = useState({
-        width: undefined,
-        height: undefined,
-    });
-
-	
-    useEffect(() => {
-        const handleResize = () => {
-            setSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
-        window.addEventListener("resize", handleResize);
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    useEffect(() => {
-        if (size.width > 768 && menuOpen) {
-            setMenuOpen(false);
-        }
-    }, [size.width, menuOpen]);
-
-    const menuToggleHandler = () => {
-        setMenuOpen((p) => !p);
-    };
-
-
 	return(
+		<header className={"fixed-top " + styles.header}>
+			<nav className="navbar container navbar-expand-lg navbar-light">
+				<Link className="navbar-brand" href="/">
+					<a className={styles.header__brand}><Image  src="/home/PiratesSkull.png" alt="hola" width={58} height={58}/></a>
+				</Link>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
 
-
-		<header className={styles.header}>
-			<div className={styles.header__content}>
-				<div>
-					<Link className={styles.header__brand} href="/">
-						<a><Image src="/home/PiratesSkull.png" alt="hola" width={58} height={58}/></a>
-					</Link>
-				</div>
-				<nav
-					className={styles.header__nav }
-				>
-					<div>
-						<ul>
-							<li>
-								<Link href="/"><a className="">MINT</a></Link>
+				<ul className=" navbar-nav  mt-2 mt-lg-0">
+							<li clasName="nav-item">
+								<Link href="/"><a className={styles.header__link}>MINT</a></Link>
 							</li>
-							<li>
-								<Link href="/"><a className="">ROADMAP</a></Link>
+							<li clasName="nav-item">
+								<Link href="/"><a className={styles.header__link}>ROADMAP</a></Link>
 							</li>
-							<li>
-								<Link href="/"><a className="">CREW</a></Link>
+							<li clasName="nav-item">
+								<Link href="/"><a className={styles.header__link}>CREW</a></Link>
 							</li>
-							<li>
-								<Link href="/"><a className="">JOIN US</a></Link>
+							<li clasName="nav-item">
+								<Link href="/"><a className={styles.header__link}>JOIN US</a></Link>
 							</li>
-							<button >Connect</button>
 						</ul>
-					</div>
-				</nav>
-				<div className={styles.header__toggle}>
-                    {!menuOpen ? (
-                        <BiMenuAltRight onClick={menuToggleHandler} />
-                    ) : (
-                        <AiOutlineClose onClick={menuToggleHandler} />
-                    )}
-                </div>
+				<button  hidden className="btn btn-outline-success my-2 my-sm-0" type="submit">Connect</button>
 			</div>
+			</nav>
 		</header>
+
 
 
 
@@ -89,24 +49,3 @@ const Navigation = () => {
 
 export default Navigation;
 
-
-
-/*
-
-
-		{<nav className="menu nav-bar navbar-expand-md">
-			<div className="container">
-				<Link class="navbar-brand" href="/"><a><Image src="/home/PiratesSkull.png" alt="hola" width={100} height={100}/></a></Link>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse">
-					<div className="navbar-nav ms-auto d-flex justify-content-evenly ">
-						<Link href="/mint/"><a className={""/*styles.navlink}>MINT</a></Link>
-						<Link href="/"><a className={""/*styles.navlink}>ROADMAP</a></Link>
-						<Link href="/"><a className={""/*styles.navlink}>CREW</a></Link>
-						<Link href="/"><a className={""/*styles.navlink}>JOIN US</a></Link>
-					</div>
-				</div>
-			</div>
-		</nav>}*/
